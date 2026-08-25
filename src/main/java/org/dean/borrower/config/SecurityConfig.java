aec8e43a8f02cf1b6ba37f6154cfe8f00fd651fc
Dean <deannadelacruz10@gmail.com>
2026-08-25 20:16:03 +0800

    override security measures for now

--- src/main/java/org/dean/borrower/config/SecurityConfig.java
+++ src/main/java/org/dean/borrower/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package org.dean.borrower.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +13,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //bean to override any security measures for now
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth ->auth
+                .requestMatchers("/api/auth/signup").permitAll()
+                        .anyRequest().permitAll());
+
+        return httpSecurity.build();
+
     }
 }
