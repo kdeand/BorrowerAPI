aa6551eebb84a4ea85285b4b44916ad8fb5fd592
Dean <deannadelacruz10@gmail.com>
2026-08-29 16:26:12 +0800

    get header

---
+++ src/main/java/org/dean/borrower/Auth/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package org.dean.borrower.Auth;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.dean.borrower.repository.UserRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //getheader
+        String authHeader = request.getHeader("Authorization");
+
+        //if header is null, and if it does not contain a bearer
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        //extract email from the token
+    }
+}
